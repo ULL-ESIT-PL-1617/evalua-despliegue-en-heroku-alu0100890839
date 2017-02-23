d83ff93bfe847ed90c412c9113da22a8a1c80bd8
olbapjenkins <olbapp@hotmail.es>
2017-02-23 09:07:10 +0000

    port changed

--- scripts/server.js
+++ scripts/server.js
@@ -3,8 +3,9 @@ var app = express()
 var path = require('path');
 
 app.use(express.static('gh-pages'));
+app.set('port', (process.env.PORT || 5000));
 
-var server = app.listen(80, function() {
+var server = app.listen(app.get('port'), function() {
   var host = server.address().address
   var port = server.address().port
 
